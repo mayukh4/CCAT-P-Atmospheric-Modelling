ae3d9306bae7bb27027ee14bdc6a4506a008fca2
MAYUKH BAGCHI <mayukh.bagchi@queensu.ca>
2023-05-04 14:35:42 -0400

    Add files via upload

---
+++ 850GHz_skydips/ammodelCCAT_annual.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+rm submm_load_CCAT_350um_annual_5_skydip.out
+rm submm_load_CCAT_350um_annual_50_skydip.out
+rm submm_load_CCAT_350um_annual_95_skydip.out
+
+for Z in $(seq 5.6 1.0 5.6); do
+  printf "%4.1f" $Z
+  for ZA in $(seq 5.0 5.0 85.0); do
+    Trj=$(am submm_load_CCAT_350um_annual_5.amc $Z km $ZA deg 2>/dev/null)
+    printf " %e" $Trj
+    echo "$Z $ZA $Trj" >> submm_load_CCAT_350um_annual_5_skydip.out
+  done
+  printf "\n"
+done
+
+for Z in $(seq 5.6 1.0 5.6); do
+  printf "%4.1f" $Z
+  for ZA in $(seq 5.0 5.0 85.0); do
+    Trj=$(am submm_load_CCAT_350um_annual_50.amc $Z km $ZA deg 2>/dev/null)
+    printf " %e" $Trj
+    echo "$Z $ZA $Trj" >> submm_load_CCAT_350um_annual_50_skydip.out
+  done
+  printf "\n"
+done
+
+for Z in $(seq 5.6 1.0 5.6); do
+  printf "%4.1f" $Z
+  for ZA in $(seq 5.0 5.0 85.0); do
+    Trj=$(am submm_load_CCAT_350um_annual_95.amc $Z km $ZA deg 2>/dev/null)
+    printf " %e" $Trj
+    echo "$Z $ZA $Trj" >> submm_load_CCAT_350um_annual_95_skydip.out
+  done
+  printf "\n"
+done
